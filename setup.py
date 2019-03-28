8f015201f5e81a5e11b68f89f1e8cb9cea785768
John Jung <jej@moss.lib.uchicago.edu>
2019-03-28 09:42:03 -0500

    Renamed directories.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='marc_tools',
+    name='metadata_tools',
     version='0.0.1',
     author='John Jung',
     author_email='jej@uchicago.edu',
-    description='Scripts to convert MARC data to different formats',
+    description='Scripts to convert metadata to and from different formats.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/johnjung/marc_tools',
+    url='https://github.com/johnjung/metadata_tools',
     packages=setuptools.find_packages()
 )
