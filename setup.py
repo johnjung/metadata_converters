640d92626c2e800b52718230b0ce4e6797842c07
John Jung <john@fieldrotation.com>
2019-05-12 08:39:24 -0500

    update package name.

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='metadata_tools',
+    name='metadata_converters',
     version='0.0.1',
     author='John Jung',
     author_email='jej@uchicago.edu',
     description='Scripts to convert metadata to and from different formats.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/johnjung/metadata_tools',
+    url='https://github.com/johnjung/metadata_converters',
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
-            'marc2dc = metadata_tools.marc2dc:main',
-            'marc2schemadotorg = metadata_tools.marc2schemadotorg:main'
+            'marc2dc = metadata_converters.marc2dc:main',
+            'marc2schemadotorg = metadata_converters.marc2schemadotorg:main'
         ]
     },
     install_requires=[
