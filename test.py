ea2bdaa471ccd7762792c75cd0106fb94def108b
Jose Reyes <reyesj5@UCHICAGO.EDU>
2019-06-19 11:01:14 -0500

    added MarcXmlConverter test cases

--- test.py
+++ test.py
@@ -1,54 +1,82 @@
 import unittest
+from pathlib import Path
 from metadata_converters import MarcXmlConverter, MarcToDc, MarcXmlToSchemaDotOrg
 
 
 class TestMarcXmlConverter(unittest.TestCase):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Create test objects. Test data should be placed in the test_data
         directory of this project. It should include sample data from different
         digital collections projects, and the data should be varied enough to 
         be able to catch different kinds of edge cases in testing."""
         super().__init__(*args, **kwargs)
-        raise NotImplementedError
+        self.test_init()
 
     def test_init(self):
         """MarcXmlConverter should have a .record property which is a
-        xml.etree.ElementTree."""
-        raise NotImplementedError
+        xml.etree.ElementTree.Element."""
+        file = Path('test_data/sample_record_01.xml')
+        with open(file, 'r') as f:
+            data = f.read().replace('\n', '')
+            self.assertTrue(len(data) > 0)
+        self.collection = MarcXmlConverter(data)
+        self.assertEqual(str(type(self.collection.record)), "<class 'xml.etree.ElementTree.Element'>")
+        #self.assertEqual(self.collection.get_marc_field('655', '', '', ''), ['Thematic maps.', 'lcgft'])
 
     def test_get_marc_field_field_tag(self):
         """Retrieve a marc data field and a control field using the
         get_marc_field() method."""
-        raise NotImplementedError 
+        self.assertEqual(self.collection.get_marc_field('003', '', '', ''), ['OLE'])
+        self.assertEqual(self.collection.get_marc_field('003', '5', '', ''), ['OLE'])
+        self.assertEqual(self.collection.get_marc_field('003', '', '5', ''), ['OLE'])
+        self.assertEqual(self.collection.get_marc_field('003', '', '', '5'), ['OLE'])
+        self.assertEqual(self.collection.get_marc_field('651', '', '', ''),  ['Illinois', 'Chicago.', 'fast', '(OCoLC)fst01204048'])
 
     def test_get_marc_field_subfield(self):
         """Retrieve a marc data field and subfield using the get_marc_field()
         method."""
-        raise NotImplementedError 
+        self.assertEqual(self.collection.get_marc_field('650', '[a]', '', '4'), [])
+        self.assertEqual(self.collection.get_marc_field('650', '[a]', '4', ''), [])
+        self.assertEqual(self.collection.get_marc_field('650', '[a]', '', '')[0], 'Ethnology')
 
     def test_get_marc_field_indicator_1(self):
         """Retrieve a record with the first indicator marked using the
         get_marc_field() method."""
-        raise NotImplementedError 
+        self.assertEqual(self.collection.get_marc_field('', '', '2', '4'), [])
+        self.assertEqual(self.collection.get_marc_field('', '', '2', ''), [])
+        self.assertEqual(self.collection.get_marc_field('650', '[a]', '4', ''), [])
+        self.assertEqual(self.collection.get_marc_field('245', '', '1', ''), ['Census tracts of Chicago, 1940', 'Races and nationalities.'])
 
     def test_get_marc_field_indicator_2(self):
         """Retrieve a record with the second indicator marked using the
         get_marc_field() method."""
-        raise NotImplementedError 
+        self.assertEqual(self.collection.get_marc_field('', '', '', 's'), [])
+        self.assertEqual(self.collection.get_marc_field('655', '[a]', '4', '7'), [])
+        self.assertEqual(self.collection.get_marc_field('830', '', '', '0'), ['Social scientists map Chicago.', 'ICU', 'University of Chicago Digital Preservation Collection.', 'ICU'])
 
 
 class TestMarcToDc(unittest.TestCase):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Create test objects. Test data should be placed in the test_data
         directory of this project. It should include sample data from different
         digital collections projects, and the data should be varied enough to 
         be able to catch different kinds of edge cases in testing."""
         super().__init__(*args, **kwargs)
-        raise NotImplementedError
+        self.test_init()
 
+    def test_init(self):
+        """MarcToDc extends the MarcXmlConverter class and should have a .record property which is a
+        xml.etree.ElementTree.Element."""
+        file = Path('test_data/sample_record_02.xml')
+        with open(file, 'r') as f:
+            data = f.read().replace('\n', '')
+            self.assertTrue(len(data) > 0)
+        self.collection = MarcToDc(data)
+'''
     def test_get_rights_access(self):
         """Be sure the object can return the DC element."""
-        raise NotImplementedError
+        print(self.collection.__str__())
+        self.assertEqual(self.collection.__getattr__('type'), 'sf')
 
     def test_get_contributor(self):
         """Be sure the object can return the DC element."""
@@ -133,3 +161,7 @@ class TestMarcToDc(unittest.TestCase):
     def test_get_type(self):
         """Be sure the object can return the DC element."""
         raise NotImplementedError
+'''
+
+if __name__ == '__main__':
+    unittest.main()
