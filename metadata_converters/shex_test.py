3f2931a2496265fdf2bf5c90c4cf94a7f5b13774
John Jung <john@fieldrotation.com>
2019-11-01 13:06:12 -0500

    First draft for EDM ShEx validation.

---
+++ metadata_converters/shex_test.py
@@ -0,0 +1,41 @@
+import sys
+from pyshex.shex_evaluator import ShExEvaluator
+from rdflib import URIRef
+
+
+if __name__=="__main__":
+    with open('ssmaps.ttl') as f:
+        rdf = f.read()
+    
+    with open('shex/uchicago_library_ssmaps.shex') as f:
+        shex = f.read()
+
+    # rewrite this:
+    # find every subject in the graph.
+    # get the shape that corresponds to that subject's type.
+    # validate each one in turn. 
+    
+    for shape, focus in (
+                            (
+                                URIRef('https://www.lib.uchicago.edu/WebResource'),
+                                URIRef('file:///digital_collections/IIIF_Files//social_scientists_maps/G4104-C6-1933-U5-d.tif')
+                            ),
+                            (
+                                URIRef('https://www.lib.uchicago.edu/proxy'),
+                                URIRef('file:///digital_collections/IIIF_Files/social_scientists_maps/G4104-C6-1933-U5-d/G4104-C6-1933-U5-d.dc.xml')
+                            )
+                        ):
+        evaluator = ShExEvaluator(
+            focus=focus,
+            rdf=rdf,
+            schema=shex,
+            start=shape
+        )
+        results = evaluator.evaluate()
+        for result in results:
+            if result.result == False:
+                print('ERROR')
+                print('FOCUS: {}'.format(result.focus))
+                print('START: {}'.format(result.start))
+                print('REASON: {}'.format(result.reason.strip()))
+                print('')
