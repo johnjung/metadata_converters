09ee13c486d5bb533974b56ac380da1783656d9d
John Jung <jej@moss.lib.uchicago.edu>
2019-12-18 14:24:54 -0600

    Modified setup.py.

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ setuptools.setup(
     version='0.0.1',
     author='John Jung',
     author_email='jej@uchicago.edu',
-    data_files=[('metadata_converters/json', ['socscimaps_marc2dc.json'])],
     description='Scripts to convert metadata to and from different formats.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/johnjung/metadata_converters',
     packages=setuptools.find_packages(),
+    package_data={
+        'metadata_converters': ['metadata_converters/json/*.json']
+    },
     entry_points={
         'console_scripts': [
             'marc2dc = metadata_converters.marc2dc:main',
